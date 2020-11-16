7901fad09a411025d7b8e4ee4e446d9b4c1dd838
ma22npk <gunjo64@icloud.com>
2020-11-16 20:26:59 +0900

    ドットインストール開始

---
+++ dot_Install_study/src/dot_Install_study/DotInstall_01.java
@@ -0,0 +1,9 @@
+package dot_Install_study;
+
+public class DotInstall_01 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        System.out.println("ドットインストールの学習開始");
+    }
+}
