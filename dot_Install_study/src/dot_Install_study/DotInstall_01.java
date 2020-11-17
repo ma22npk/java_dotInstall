58d5123379d02cce98ece1ae120ac55b275ac867
ma22npk <gunjo64@icloud.com>
2020-11-17 23:13:19 +0900

    文字の編集

--- dot_Install_study/src/dot_Install_study/DotInstall_01.java
+++ dot_Install_study/src/dot_Install_study/DotInstall_01.java
@@ -3,7 +3,9 @@ package dot_Install_study;
 public class DotInstall_01 {
 
     public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
         System.out.println("ドットインストールの学習開始します。");
-        System.out.println("ドットインストールの学習開始します。");
+        System.out.println("ハロー田口");
+        System.out.println("イエーイ田口");
     }
 }
